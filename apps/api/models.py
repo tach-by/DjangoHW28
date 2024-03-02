5f23b303ca5297ff4153f8b4c5b7418a610267d8
tach <tachterry@gmail.com>
2024-03-03 00:47:09 +0300

    add models

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
