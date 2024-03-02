ec5c4ca043e6c27d9d2de1f7ee3e74f430955759
tach <tachterry@gmail.com>
2024-03-02 23:41:31 +0300

    add serializers

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -6,3 +6,43 @@ from apps.product.models import Product, Rating, Comment
 # Далее, создайте отдельный Serializer для получения рейтинга товара с его комментариями.
 # В этом Serializer реализуйте метод, который позволит пользователю ввести идентификатор товара,
 # а затем вернет его рейтинг и список всех комментариев, связанных с этим товаром.
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class ProductRatingCommentSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField()
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Product does not exist.")
+        return value
+
+    def get_product_rating_and_comments(self, validated_data):
+        product_id = validated_data['product_id']
+        product = Product.objects.get(id=product_id)
+        ratings = Rating.objects.filter(product=product)
+        comments = Comment.objects.filter(product=product)
+        rating_serializer = RatingSerializer(ratings, many=True)
+        comment_serializer = CommentSerializer(comments, many=True)
+        return {
+            'product': ProductSerializer(product).data,
+            'ratings': rating_serializer.data,
+            'comments': comment_serializer.data
+        }
